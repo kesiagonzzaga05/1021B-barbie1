2a61809e36724eae10c37d0c3bf100b2313d01f1
kesiagonzzaga05 <kesia.gonzaga@estudante.ifms.edu.br>
2023-08-31 10:17:24 -0400

    atualizado

---
+++ src/componentes/main/Main.tsx
@@ -0,0 +1,19 @@
+import Filme from "../filme/Filme";
+
+export default function Main(){
+    return(
+        <main>
+            <main className="content-main">
+                 <Filme titulo="barbie" sinopse=" filme chato da barbie:
+                Depois de ser expulsa da Barbieland por ser uma boneca de aparência 
+                menos do que perfeita, Barbie parte para o mundo humano em busca da 
+                verdadeira felicidade." imagem="/barbie.png"/>
+           
+                <Filme titulo="barbie" sinopse=" filme chato da barbie:
+                Depois de ser expulsa da Barbieland por ser uma boneca de aparência 
+                menos do que perfeita, Barbie parte para o mundo humano em busca da 
+                verdadeira felicidade." imagem="/barbie.png"/>
+             </main>
+        </main>
+    )
+}
