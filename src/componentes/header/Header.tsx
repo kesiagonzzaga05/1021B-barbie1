9b6447059178a7de18547177ea0694a1c0cb694a
kesiagonzzaga05 <kesia.gonzaga@estudante.ifms.edu.br>
2023-08-31 08:56:29 -0400

    meu primeiro commit

---
+++ src/componentes/header/Header.tsx
@@ -0,0 +1,32 @@
+import './Header.css'
+function Header(){
+        return(
+            <header>
+              <div>
+                Logo
+              </div>
+              <nav>
+                <ul>
+                    <li>
+                        <a href="#">Página inicial</a>
+                    </li>
+
+                    <li>
+                        <a href="#">sobre</a>
+                    </li>  
+
+                    <li>
+                     <a href="#">noticia</a>
+                    </li>
+                    
+                       <li>
+                        <a href="#">contato</a>
+                       </li>
+                        
+                   
+                </ul>
+              </nav>
+            </header>
+        )
+}
+export default Header
